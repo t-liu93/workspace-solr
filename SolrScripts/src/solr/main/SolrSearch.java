9fd7490aaa193255c8fdafabf739d90e3da7badb
Felipe Ebert <felipe.ebert@gmail.com>
2016-12-23 15:57:36 +0100

    Added Constants class.

--- SolrScripts/src/solr/main/SolrSearch.java
+++ SolrScripts/src/solr/main/SolrSearch.java
@@ -13,43 +13,47 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
+import solr.utils.Constants;
+
 public class SolrSearch {
-	
-	
 
-	public static void main(String[] args) {
-
+	/**
+	 * This method counts the occurrences of each feature in the framework and
+	 * save the results in a csv file.
+	 * 
+	 * @param framework
+	 */
+	public static void countFeaturesOccurrences(String framework) {
 		try {
 
-			List<String> content = Files.readAllLines(Paths.get("./framework/meta.txt"));
+			List<String> content = Files.readAllLines(Paths.get(framework));
+
 			for (String string : content) {
 				String x = string;
 				System.out.println(x);
 			}
-			
-			String urlString = "http://localhost:8983/solr/gettingstarted";
-			SolrClient solr = new HttpSolrClient.Builder(urlString).build();
-			
+
+			SolrClient solr = new HttpSolrClient.Builder(Constants.URL_SORL).build();
 
 			SolrQuery query = new SolrQuery();
 
 			query.setQuery("messages.message:/.*[Ii]t appears.*/");
-			query.setFields("_number", "messages.id", "messages.message");
+			query.setFields(Constants._NUMBER, Constants.MESSAGES_ID, Constants.MESSAGES_MESSAGE);
 			query.setStart(0);
-			
+
 			QueryResponse response = solr.query(query);
-			
+
 			SolrDocumentList results = response.getResults();
-			
+
 			long numCodeReviewsFound = results.getNumFound();
 			System.out.println(numCodeReviewsFound);
-			
+
 			for (int i = 0; i < results.size(); ++i) {
 				SolrDocument d = results.get(i);
 
 				@SuppressWarnings("unchecked")
-				List<String> dm = ((List<String>) d.getFieldValue("messages.message"));
-				
+				List<String> dm = ((List<String>) d.getFieldValue(Constants.MESSAGES_MESSAGE));
+
 				for (String string : dm) {
 					if (string.contains("like a reasonable")) {
 						String s = string;
@@ -65,5 +69,21 @@ public class SolrSearch {
 		} catch (SolrServerException | IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * This is the main method which makes use of all Solr methods.
+	 * 
+	 * @param args
+	 *            Unused.
+	 * 
+	 */
+	public static void main(String[] args) {
+
+		// the path for the framework file
+		String framework = "./framework/meta.txt";
+
+		// count the occurrences
+		countFeaturesOccurrences(framework);
 	}
 }
---
+++ SolrScripts/src/solr/utils/Constants.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package solr.utils;
+
+/**
+ * <h1>Constants</h1>
+ * 
+ * The Constants class contains all constants used in the program.
+ *
+ * @author Felipe Ebert
+ * @version 0.1
+ * @since 2016-12-23
+ */
+public class Constants {
+
+	public static String URL_SORL = "http://localhost:8983/solr/gettingstarted";
+	
+	public static String _NUMBER = "_number";
+	
+	public static String MESSAGES_ID = "messages.id";
+	
+	public static String MESSAGES_MESSAGE = "messages.message";
+}
