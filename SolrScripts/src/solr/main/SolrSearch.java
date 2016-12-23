66e6e61ad84e6673d0ea1b997fbda5fd4a4f90d0
Felipe Ebert <felipe.ebert@gmail.com>
2016-12-23 15:33:37 +0100

    Adding some models of the framework.

--- SolrScripts/src/solr/main/SolrSearch.java
+++ SolrScripts/src/solr/main/SolrSearch.java
@@ -1,6 +1,7 @@
 package solr.main;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
@@ -15,26 +16,43 @@ public class SolrSearch {
 	public static void main(String[] args) {
 
 		try {
+
 			
 			String urlString = "http://localhost:8983/solr/gettingstarted";
 			SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+			
 
 			SolrQuery query = new SolrQuery();
-			
-			// double check it
-			query.setRequestHandler("/spellCheckCompRH");
-			
-			query.setQuery("messages.message:/.*it appears.*/");
-			query.setFields("id", "_number");
+
+			query.setQuery("messages.message:/.*[Ii]t appears.*/");
+			query.setFields("_number", "messages.id", "messages.message");
 			query.setStart(0);
 			
 			QueryResponse response = solr.query(query);
+			
 			SolrDocumentList results = response.getResults();
+			
+			long numCodeReviewsFound = results.getNumFound();
+			System.out.println(numCodeReviewsFound);
+			
 			for (int i = 0; i < results.size(); ++i) {
 				SolrDocument d = results.get(i);
+
+				@SuppressWarnings("unchecked")
+				List<String> dm = ((List<String>) d.getFieldValue("messages.message"));
+				
+				for (String string : dm) {
+					if (string.contains("like a reasonable")) {
+						String s = string;
+						System.out.println(s);
+					}
+				}
+
+				System.out.println(dm);
+
 				System.out.println(d);
 			}
-			
+
 		} catch (SolrServerException | IOException e) {
 			e.printStackTrace();
 		}
