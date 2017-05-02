6d8e38071877d551df71a802ef25ce085215c318
Felipe Ebert <felipe.ebert@gmail.com>
2017-05-02 17:19:21 +0200

    Added the CompileAllResults class. To start yet.

---
+++ SolrScripts/src/solr/main/CompileAllResults.java
@@ -0,0 +1,13 @@
+package solr.main;
+
+public class CompileAllResults {
+
+	public static void compileAllResults() {
+		// TODO: all-ID.txt, all-TuplesID and inline-results.csv
+	}
+
+	public static void main(String[] args) {
+
+		compileAllResults();
+	}
+}
